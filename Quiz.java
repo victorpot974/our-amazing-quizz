9b9e70bf0f04f9a1f5b3798a538f594bcf739d09
Dawijak04 <23384549@studentmail.ul.ie>
2023-10-23 15:56:43 +0100

    Updated quiz

--- Quiz.java
+++ Quiz.java
@@ -126,6 +126,7 @@ public class Quiz implements ActionListener{
 	//Frame
 	JFrame frame = new JFrame();
 	JTextField textfield = new JTextField();
+	JTextField textfield2 = new JTextField();
 	JTextArea textarea = new JTextArea();
 	
 	//Buttons
@@ -161,78 +162,81 @@ public class Quiz implements ActionListener{
 		
 		//Frame
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setSize(650,650);
+		frame.setSize(1600,1000);
 		frame.getContentPane().setBackground(new Color(50,50,50));
 		frame.setLayout(null);
 		frame.setResizable(false);
 		
 		//Title banner/Heading 1
-		textfield.setBounds(0,0,650,50);
+		textfield.setBounds(0,0,1600,100);
 		textfield.setBackground(new Color(25,25,25));
-		textfield.setForeground(new Color(25,255,0));
+		textfield.setForeground(new Color(37,49,220));
 		textfield.setFont(new Font("Ink Free", Font.PLAIN,30));
 		textfield.setBorder(BorderFactory.createBevelBorder(1));
 		textfield.setHorizontalAlignment(JTextField.CENTER);
 		textfield.setEditable(false);
 		
+		
 		//Question banner/Heading 2
-		textarea.setBounds(0,50,650,50);
+		textarea.setBounds(0,100,1600,100);
 		textarea.setLineWrap(true);
 		textarea.setWrapStyleWord(true);
 		textarea.setBackground(new Color(25,25,25));
-		textarea.setForeground(new Color(25,255,0));
+		textarea.setForeground(new Color(37,49,220));
 		textarea.setFont(new Font("MV Boli",Font.PLAIN,25));
 		textarea.setBorder(BorderFactory.createBevelBorder(1));
+		//textarea.setHorizontalAlignment(JTextArea.CENTER);
 		textarea.setEditable(false);
 		
+		
 		//Button A
-		buttonA.setBounds(0,100,100,100); //Size and location
+		buttonA.setBounds(0,270,100,100); //Size and location
 		buttonA.setFont(new Font("MV Boli",Font.BOLD,35)); //Font and text size
 		//buttonA.setFocusable(false); //Not highlighted//////////////////////////////////////////
 		buttonA.addActionListener(this); //Takes user input
 		buttonA.setText("A"); //Text
 		
 		//Button B
-		buttonB.setBounds(0,200,100,100);//Size and location
+		buttonB.setBounds(0,400,100,100);//Size and location
 		buttonB.setFont(new Font("MV Boli",Font.BOLD,35)); //Font and text size
 		//buttonB.setFocusable(false); //Not highlighted//////////////////////////////////////////
 		buttonB.addActionListener(this); //Takes user input
 		buttonB.setText("B"); //Text
 		
 		//Button C
-		buttonC.setBounds(0,300,100,100); //Size and location
+		buttonC.setBounds(0,530,100,100); //Size and location
 		buttonC.setFont(new Font("MV Boli",Font.BOLD,35)); //Font and size
 		//buttonC.setFocusable(false); //Not highlighted///////////////////////////////////////////
 		buttonC.addActionListener(this); //Takes input
 		buttonC.setText("C");//Text
 		
 		//Button D
-		buttonD.setBounds(0,400,100,100); //Size and location
+		buttonD.setBounds(0,660,100,100); //Size and location
 		buttonD.setFont(new Font("MV Boli",Font.BOLD,35)); //Font and text size
 		//buttonD.setFocusable(false); //Not highlighted////////////////////////////////////////////
 		buttonD.addActionListener(this); //Takes input
 		buttonD.setText("D"); //Text
 
 		//Answer label A
-		answer_labelA.setBounds(125, 100, 500, 100); //Size and location
+		answer_labelA.setBounds(125, 270, 1600, 100); //Size and location
 		answer_labelA.setBackground(new Color(50,50,50)); //Background colour
 		answer_labelA.setForeground(new Color(25,255,0)); //Foreground colour
 		answer_labelA.setFont(new Font("MV Boli",Font.PLAIN,35)); //Font and text size
 		
 		//Answer label B
-		answer_labelB.setBounds(125, 200, 500, 100); //Size and location
+		answer_labelB.setBounds(125, 400, 1600, 100); //Size and location
 		answer_labelB.setBackground(new Color(50,50,50)); //Background colour
 		answer_labelB.setForeground(new Color(25,255,0)); //Background colour
 		answer_labelB.setFont(new Font("MV Boli",Font.PLAIN,35)); //Font and text size
 		
 		//Answer label C
-		answer_labelC.setBounds(125, 300, 500, 100); //Size and location
+		answer_labelC.setBounds(125, 530, 1600, 100); //Size and location
 		answer_labelC.setBackground(new Color(50,50,50)); //Background colour
 		answer_labelC.setForeground(new Color(25,255,0)); //Foreground colour
 		answer_labelC.setFont(new Font("MV Boli",Font.PLAIN,35)); //Font and text size
 		
 		//Answer label D
-		answer_labelD.setBounds(125, 400, 500, 100); //Size and location
+		answer_labelD.setBounds(125, 660, 1600, 100); //Size and location
 		answer_labelD.setBackground(new Color(50,50,50)); //Background colour
 		answer_labelD.setForeground(new Color(25,255,0)); //Foreground colour
 		answer_labelD.setFont(new Font("MV Boli",Font.PLAIN,35)); //Font and text size
@@ -240,7 +244,7 @@ public class Quiz implements ActionListener{
 		
 		//End of quiz results
 		//Score
-		number_right.setBounds(225,225,200,100); //Location and size
+		number_right.setBounds(625,300,400,200); //Location and size
 		number_right.setBackground(new Color(25,25,25)); //Background colour
 		number_right.setForeground(new Color(25,255,0)); //Foreground colour
 		number_right.setFont(new Font("Ink Free",Font.BOLD,50)); //Font and text size
@@ -248,16 +252,16 @@ public class Quiz implements ActionListener{
 		number_right.setHorizontalAlignment(JTextField.CENTER); //Centre of box
 		number_right.setEditable(false); //Not editable
 		
+
 		//Percentage
-		percentage.setBounds(225,325,200,100); //Location and size
+		percentage.setBounds(625,500,400,200); //Location and size
 		percentage.setBackground(new Color (25,25,25)); //Background colour
 		percentage.setForeground(new Color(25,255,0)); //Foreground colour
 		percentage.setFont(new Font("Ink Free",Font.BOLD,50)); //Font and text size
 		percentage.setBorder(BorderFactory.createBevelBorder(1)); //Border
 		percentage.setHorizontalAlignment(JTextField.CENTER); //Centre of box
 		percentage.setEditable(false); //Not editable
-
-		
+	
 		
 		
 		
@@ -266,7 +270,7 @@ public class Quiz implements ActionListener{
 		
 		//Buttons
 		//Button 1/Easy
-		button1.setBounds(0,100,100,100); //Location and size
+		button1.setBounds(625,210,100,100); //Location and size
 		button1.setFont(new Font("MV Boli",Font.BOLD,35)); //Font and text size
 		//button1.setFocusable(false);////////////////////////////////////////////////////
 		button1.setText("1"); //Text
@@ -283,7 +287,7 @@ public class Quiz implements ActionListener{
 		});
 		
 		//Button 2/Medium
-		button2.setBounds(0,200,100,100); //Location and size
+		button2.setBounds(625,330,100,100); //Location and size
 		button2.setFont(new Font("MV Boli",Font.BOLD,35)); //Font and text size
 		//button2.setFocusable(false);////////////////////////////////////////////////////////
 		button2.setText("2"); //Text
@@ -299,7 +303,7 @@ public class Quiz implements ActionListener{
 		});
 		
 		//Button 3/Hard
-		button3.setBounds(0,300,100,100); //Location and size
+		button3.setBounds(625,450,100,100); //Location and size
 		button3.setFont(new Font("MV Boli",Font.BOLD,35)); //Font and text size
 		//button3.setFocusable(false);//////////////////////////////////////////////////////////////
 		button3.setText("3"); //Text
@@ -315,7 +319,7 @@ public class Quiz implements ActionListener{
 		});
 		
 		//Button 4/Random  
-		button4.setBounds(0,400,100,100); //Location and size
+		button4.setBounds(625,570,100,100); //Location and size
 		button4.setFont(new Font("MV Boli",Font.BOLD,35)); //Font and text size
 		//button4.setFocusable(false);///////////////////////////////////////////////////////////////
 		button4.setText("4"); //Text
@@ -329,7 +333,7 @@ public class Quiz implements ActionListener{
 		});
 
 		//Button 5/Increasing Difficulty  !This method continues later in the code!
-		button5.setBounds(0,500,100,100); //Location and size
+		button5.setBounds(625,690,100,100); //Location and size
 		button5.setFont(new Font("MV Boli",Font.BOLD,35)); //Font and text size
 		//button5.setFocusable(false);////////////////////////////////////////////////////////////////
 		button5.setText("5"); //Text
@@ -347,37 +351,37 @@ public class Quiz implements ActionListener{
 		
 		//Answer Labels for difficulty modes
 		//Label 1
-		answer_label1.setBounds(125, 100, 500, 100); //Location and size
+		answer_label1.setBounds(750, 210, 500, 100); //Location and size
 		answer_label1.setBackground(new Color(50,50,50)); //Background colour
 		answer_label1.setForeground(new Color(25,255,0)); //Foreground colour
 		answer_label1.setFont(new Font("MV Boli",Font.PLAIN,35)); //Font and text size
 		
 		//Label 2
-		answer_label2.setBounds(125, 200, 500, 100); //Location and size
+		answer_label2.setBounds(750, 330, 500, 100); //Location and size
 		answer_label2.setBackground(new Color(50,50,50)); //Background colour
-		answer_label2.setForeground(new Color(25,255,0)); //Foreground colour
+		answer_label2.setForeground(new Color(255,240,20)); //Foreground colour
 		answer_label2.setFont(new Font("MV Boli",Font.PLAIN,35)); //Font and text size
 		
 		//Label 3
-		answer_label3.setBounds(125, 300, 500, 100); //Location and size 
+		answer_label3.setBounds(750, 450, 500, 100); //Location and size 
 		answer_label3.setBackground(new Color(50,50,50)); //Background colour
-		answer_label3.setForeground(new Color(25,255,0)); //Foreground colour
+		answer_label3.setForeground(new Color(255,44,20)); //Foreground colour
 		answer_label3.setFont(new Font("MV Boli",Font.PLAIN,35)); //Font and text size
 		
 		//Label 4
-		answer_label4.setBounds(125, 400, 500, 100); //Location and size
+		answer_label4.setBounds(750, 570, 500, 100); //Location and size
 		answer_label4.setBackground(new Color(50,50,50)); //Background colour
-		answer_label4.setForeground(new Color(25,255,0)); //Foreground colour
+		answer_label4.setForeground(new Color(20,255,240)); //Foreground colour
 		answer_label4.setFont(new Font("MV Boli",Font.PLAIN,35)); //Font and text size
 		
 		//Label 5
-		answer_label5.setBounds(125, 500, 500, 100); //Location and size
+		answer_label5.setBounds(750, 690, 500, 100); //Location and size
 		answer_label5.setBackground(new Color(50,50,50)); //Background colour
-		answer_label5.setForeground(new Color(25,255,0)); //Foreground colour
+		answer_label5.setForeground(new Color(255,169,20)); //Foreground colour
 		answer_label5.setFont(new Font("MV Boli",Font.PLAIN,35)); //Font and text size
 		
 		
-		//Everthing is added to the frame 
+		//Everything is added to the frame 
 		//Headings
 		frame.add(textarea); 
 		frame.add(textfield); 
