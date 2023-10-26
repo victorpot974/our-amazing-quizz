2f85874011a04206cc8e46f98691147294a9f0cf
Dawijak04 <23384549@studentmail.ul.ie>
2023-10-26 13:46:51 +0100

    Full quiz

---
+++ epicFile/MAIN.java
@@ -0,0 +1,12 @@
+package epicFile;
+
+import epicFile.QUIZ;
+
+public class MAIN {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		QUIZ quiz = new QUIZ();
+	}
+
+}
